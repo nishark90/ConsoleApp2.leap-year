a7015728c28f230f73ac7b6c5e1f4ea21c5eda40
Nisha R K <55803996+nishark90@users.noreply.github.com>
2025-03-02 20:15:49 +0530

    Add project files.

---
+++ ConsoleApp2.leap  year/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.leap__year
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the year");   
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0)
+            {
+                Console.WriteLine("Leap year");
+            }
+            else
+            {
+                Console.WriteLine("Not Leap year");
+            }
+                Console.ReadKey();   
+        }
+    }
+}
